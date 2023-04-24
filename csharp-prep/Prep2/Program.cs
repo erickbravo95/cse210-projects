bc8ee0eb2ff768320c506c27d0a1d7db6ad6dc07
erickbravo95 <erickbravo95@gmail.com>
2023-04-24 11:20:12 -0500

    Grades Update

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string grade = Console.ReadLine();
+        int gradeReal = int.Parse(grade);
+        string letter = "";
+        if (gradeReal >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeReal >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeReal >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeReal >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (gradeReal >= 70)
+        {
+            Console.WriteLine("Congratulations you pass!");
+        }
+        else
+        {
+            Console.WriteLine("Good luck next time");
+        }
     }
 }
