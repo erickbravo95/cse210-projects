6ecfc3f5a26262838574b940e6ee129f0c7b0ad6
erickbravo95 <erickbravo95@gmail.com>
2023-04-25 10:20:54 -0500

    sum, averange and larest Update

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> lista = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int numberReal = -1;
+        do
+        {
+            Console.Write("Enter number: ");
+            string number = Console.ReadLine();
+            numberReal = int.Parse(number);
+            if (numberReal != 0)
+            {
+               lista.Add(numberReal); 
+            }
+        } while(!(numberReal == 0));
+
+        int sum = 0;
+        foreach (int i in lista)
+        {   
+            sum = sum + i;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        double ave = lista.Average();
+        Console.WriteLine($"The average is: {ave}");
+        int large = lista.Max();
+        Console.WriteLine($"The largest number is: {large}");
     }
 }
