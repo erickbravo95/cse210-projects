0afc868a85bc3a5e728e2955cee00a1be486454a
erickbravo95 <erickbravo95@gmail.com>
2023-06-24 17:36:34 -0500

    Shapes Update

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+     
+        List<Shape> shapes = new List<Shape>();
+
+        Square cuadrado = new Square("blue",5);  
+        shapes.Add(cuadrado);
+        Rectangle rectangulo = new Rectangle("red",5,10);
+        shapes.Add(rectangulo);
+        Circle circulo = new Circle("Yellow",7);
+        shapes.Add(circulo);
+
+        foreach (Shape forma in shapes){
+            Console.WriteLine($"The {forma.GetColor()} shape has an area of {forma.GetArea()}.");
+        }
+
     }
 }
